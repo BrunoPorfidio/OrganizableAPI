da6815a3e3b70fb52b8bd251ee532dbd377b6a2d
BrunoPorfidio <thewarriours7@gmail.com>
2024-08-16 02:17:41 -0300

    Implementation: Response Message.

--- src/main/java/com/brunoporfidio/organizable/dto/Message.java
+++ src/main/java/com/brunoporfidio/organizable/dto/Message.java
@@ -2,20 +2,12 @@ package com.brunoporfidio.organizable.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 @Data
 @AllArgsConstructor
 public class Message {
     
     private String Message;
-    
-//    private Message(String user_not_found){
-//    }
-//    
-//    public static ResponseEntity<String> getResponseEnity(String message, HttpStatus httpStatus){
-//        return new ResponseEntity<String>("Message : " + message, httpStatus);
-//    }
+
     
 }
